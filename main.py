954799a56eb2656acd3e69987509e2c86c2185fd
cantoneseslang <102935113+cantoneseslang@users.noreply.github.com>
2023-06-12 14:36:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -767,7 +767,7 @@ def lineBot():
                 
             if any(word in userMessage for word in NG_KEYWORDS):
                 headMessage = headMessage + NG_MESSAGE
-            headMessage = headMessage + "以下の文書を広東語口語に翻訳してください。"
+            headMessage = headMessage + "以下のユーザーの言葉を広東語口語に翻訳しそのまま返してください：{user_input}"
           
             if 'start_free_day' in user:
                 if (nowDate.date() - start_free_day.date()).days < FREE_LIMIT_DAY:
